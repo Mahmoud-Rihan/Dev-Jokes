6ee33b3161551d797022eb6edfb6bacc50cefb7b
Mahmoud Rihan <mahmoud.rihan96@gmail.com>
2024-08-12 00:12:49 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// App.js
+
+import Joke from "./Joke";
+import './App.css'
+
+function App() {
+    return (
+      <div className="container">
+      <div className="row">
+        <div className="">
+        <div className="App ">
+            <h1>Joke Generator Using React and Joke API</h1>
+            <Joke/>
+        </div>
+        </div>
+      </div>
+        </div>
+    );
+}
+
+export default App;
